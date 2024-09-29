58b85f8c702e9e48989276b95f1ac75369bc62bc
Isaiah Sherfick <isaiahsherfick8@gmail.com>
2024-09-29 15:05:50 -0500

    add ratatui example code

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,46 @@
-use std::fs::File;
-use std::io::BufReader;
-use std::io::Error;
-use rodio::{Decoder, OutputStream, source::Source};
-use std::env;
-/// Search for a pattern in a file and display the lines that contain it.
+use std::io;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let path = args.get(1).unwrap();
-    println!("path: {}", path);
-    // once we have the path, we need to load the mp3 file
-    // Get an output stream handle to the default physical sound device
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    // Load a sound from a file, using a path relative to Cargo.toml
-    let file = BufReader::new(File::open(path).unwrap());
-    println!("file: {:?}", file);
-    // Decode that sound file into a source
-    let source = Decoder::new(file).unwrap();
-    // Play the sound directly on the device
-    stream_handle.play_raw(source.convert_samples());
+use ratatui::{backend::CrosstermBackend, Terminal};
 
-    // The sound plays in a separate audio thread,
-    // so we need to keep the main thread alive while it's playing.
-    std::thread::sleep(std::time::Duration::from_secs(5));
-    println!("Where do we start?");
+use crate::{
+    app::{App, AppResult},
+    event::{Event, EventHandler},
+    handler::handle_key_events,
+    tui::Tui,
+};
+
+pub mod app;
+pub mod event;
+pub mod handler;
+pub mod tui;
+pub mod ui;
+
+#[tokio::main]
+async fn main() -> AppResult<()> {
+    // Create an application.
+    let mut app = App::new();
+
+    // Initialize the terminal user interface.
+    let backend = CrosstermBackend::new(io::stdout());
+    let terminal = Terminal::new(backend)?;
+    let events = EventHandler::new(250);
+    let mut tui = Tui::new(terminal, events);
+    tui.init()?;
+
+    // Start the main loop.
+    while app.running {
+        // Render the user interface.
+        tui.draw(&mut app)?;
+        // Handle events.
+        match tui.events.next().await? {
+            Event::Tick => app.tick(),
+            Event::Key(key_event) => handle_key_events(key_event, &mut app)?,
+            Event::Mouse(_) => {}
+            Event::Resize(_, _) => {}
+        }
+    }
+
+    // Exit the user interface.
+    tui.exit()?;
+    Ok(())
 }
