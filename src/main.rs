ed7d0948c6d12f6964d3da3deac38cacf9490b70
Isaiah Sherfick <isaiahsherfick8@gmail.com>
2025-07-17 21:01:31 -0400

    selecting file works

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,167 @@
 use eframe::egui;
+use eframe::egui::{FontFamily::*, FontId, TextStyle};
+use std::{collections::BTreeMap, fs::File};
 
 fn main() -> eframe::Result {
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([640.0, 240.0]) // wide enough for the drag-drop overlay text
+            .with_inner_size([1000.0, 240.0]) // wide enough for the drag-drop overlay text
             .with_drag_and_drop(true),
         ..Default::default()
     };
     eframe::run_native(
         "lift",
         options,
-        Box::new(|_cc| Ok(Box::<MyEguiApp>::default())),
+        Box::new(|_cc| Ok(Box::<MyEguiApp>::new(MyEguiApp::new(&_cc)))),
     )
+}
+
+enum Mode {
+    FileSelectionMode,
+    TranscribeMode,
+}
+
+impl Default for Mode {
+    fn default() -> Self {
+        Mode::FileSelectionMode
+    }
 }
 
 #[derive(Default)]
 struct MyEguiApp {
-    dropped_files: Vec<egui::DroppedFile>,
-    picked_path: Option<String>,
+    mode: Mode,
+    audio_file: Option<File>,
+    file_name: Option<String>,
+    playing: bool,
 }
 
 impl MyEguiApp {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
-        // Restore app state using cc.storage (requires the "persistence" feature).
-        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
-        // for e.g. egui::PaintCallback.
-        Self::default()
+        let text_styles: BTreeMap<TextStyle, FontId> = [
+            (TextStyle::Heading, FontId::new(20.0, Proportional)),
+            (TextStyle::Body, FontId::new(20.0, Monospace)),
+            (TextStyle::Monospace, FontId::new(12.0, Monospace)),
+            (TextStyle::Button, FontId::new(12.0, Proportional)),
+            (TextStyle::Small, FontId::new(8.0, Proportional)),
+        ]
+        .into();
+        cc.egui_ctx
+            .all_styles_mut(move |style| style.text_styles = text_styles.clone());
+        MyEguiApp {
+            mode: Mode::FileSelectionMode,
+            audio_file: None,
+            file_name: None,
+            playing: false,
+        }
     }
 }
 
 impl eframe::App for MyEguiApp {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label("Drag-and-drop files onto the window!");
-
-            if ui.button("Open file…").clicked() {
-                if let Some(path) = rfd::FileDialog::new().pick_file() {
-                    self.picked_path = Some(path.display().to_string());
-                }
-            }
-
-            if let Some(picked_path) = &self.picked_path {
-                ui.horizontal(|ui| {
-                    ui.label("Picked file:");
-                    ui.monospace(picked_path);
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        match self.mode {
+            Mode::FileSelectionMode => {
+                egui::CentralPanel::default().show(ctx, |ui| {
+                    ui.vertical_centered(|ui| {
+                        ui.add_space(40.0);
+                        ui.label("Drag-and-drop mp3 file onto this window!");
+                        if ui.button("Or click here to select a file...").clicked() {
+                            if let Some(path) = rfd::FileDialog::new().pick_file() {
+                                match File::open(&path) {
+                                    Ok(f) => {
+                                        self.audio_file = Some(f);
+                                        self.file_name = Some(String::from(
+                                            path.file_name().unwrap().to_str().unwrap(),
+                                        ));
+                                        self.mode = Mode::TranscribeMode;
+                                    }
+                                    Err(e) => {
+                                        todo!("Implement IO error popup");
+                                    }
+                                }
+                            }
+                        }
+                    });
                 });
-            }
 
-            // Show dropped files (if any):
-            if !self.dropped_files.is_empty() {
-                ui.group(|ui| {
-                    ui.label("Dropped files:");
+                preview_files_being_dropped(ctx);
 
-                    for file in &self.dropped_files {
-                        let mut info = if let Some(path) = &file.path {
-                            path.display().to_string()
-                        } else if !file.name.is_empty() {
-                            file.name.clone()
-                        } else {
-                            "???".to_owned()
-                        };
-
-                        let mut additional_info = vec![];
-                        if !file.mime.is_empty() {
-                            additional_info.push(format!("type: {}", file.mime));
+                // Collect dropped files:
+                ctx.input(|i| {
+                    if !i.raw.dropped_files.is_empty() {
+                        let path = &i.raw.dropped_files[0].path.clone().unwrap();
+                        match File::open(&path) {
+                            Ok(f) => {
+                                self.audio_file = Some(f);
+                                self.file_name =
+                                    Some(String::from(path.file_name().unwrap().to_str().unwrap()));
+                                self.mode = Mode::TranscribeMode;
+                            }
+                            Err(e) => {
+                                todo!("Implement file permission error or something");
+                            }
                         }
-                        if let Some(bytes) = &file.bytes {
-                            additional_info.push(format!("{} bytes", bytes.len()));
-                        }
-                        if !additional_info.is_empty() {
-                            info += &format!(" ({})", additional_info.join(", "));
-                        }
-
-                        ui.label(info);
                     }
                 });
             }
+            Mode::TranscribeMode => {
+                egui::CentralPanel::default().show(ctx, |ui| {
+                    ui.vertical_centered(|ui| {
+                        ui.add_space(10.0);
+                        ui.label(format!("{}", self.file_name.clone().unwrap()));
+                        let button_text = match self.playing {
+                            false => "Play",
+                            true => "Pause",
+                        };
+                        ui.columns(2, |columns| {
+                            columns[0].vertical_centered(|ui| {
+                                if ui.button(button_text).clicked() {
+                                    self.playing = !self.playing;
+                                }
+                            });
+                            columns[1].vertical_centered(|ui| {
+                                if self.playing {
+                                    ui.label("doo doo doo la la la music noises");
+                                }
+                            });
+                        });
+                    });
+                });
+            }
+        }
+    }
+}
+
+/// Preview hovering files:
+fn preview_files_being_dropped(ctx: &egui::Context) {
+    use egui::{Align2, Color32, Id, LayerId, Order, TextStyle};
+    use std::fmt::Write as _;
+
+    if !ctx.input(|i| i.raw.hovered_files.is_empty()) {
+        let text = ctx.input(|i| {
+            let mut text = "Dropping files:\n".to_owned();
+            for file in &i.raw.hovered_files {
+                if let Some(path) = &file.path {
+                    write!(text, "\n{}", path.display()).ok();
+                } else if !file.mime.is_empty() {
+                    write!(text, "\n{}", file.mime).ok();
+                } else {
+                    text += "\n???";
+                }
+            }
+            text
         });
+
+        let painter =
+            ctx.layer_painter(LayerId::new(Order::Foreground, Id::new("file_drop_target")));
+
+        let screen_rect = ctx.screen_rect();
+        painter.rect_filled(screen_rect, 0.0, Color32::from_black_alpha(192));
+        painter.text(
+            screen_rect.center(),
+            Align2::CENTER_CENTER,
+            text,
+            TextStyle::Heading.resolve(&ctx.style()),
+            Color32::WHITE,
+        );
     }
 }
